f5317a07f9931cdcacc9f9811247317a6c82968c
José Nogueira <josenogueira@loba.pt>
2018-05-25 18:16:20 +0100

    update db

--- database/zwcms_laravel.sql
+++ database/zwcms_laravel.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `zwcms_laravel` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `zwcms_laravel`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: zwcms_laravel
@@ -29,7 +27,7 @@ CREATE TABLE `migrations` (
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=89 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +36,7 @@ CREATE TABLE `migrations` (
 
 LOCK TABLES `migrations` WRITE;
 /*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2018_04_26_094806_create_testes_table',2),(4,'2018_04_27_145348_visitors',3);
+INSERT INTO `migrations` VALUES (86,'2014_10_12_000000_create_users_table',6),(87,'2014_10_12_100000_create_password_resets_table',6),(4,'2018_04_27_145348_visitors',3),(9,'2018_04_26_094806_create_testes_table',4),(10,'2018_05_22_132844_create_tests_table',4),(34,'2018_05_22_140439_create_visitors_table',5),(88,'2018_05_22_145111_create_website_settings_table',6);
 /*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,32 +65,6 @@ LOCK TABLES `password_resets` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `testes`
---
-
-DROP TABLE IF EXISTS `testes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `testes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nome` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `testes`
---
-
-LOCK TABLES `testes` WRITE;
-/*!40000 ALTER TABLE `testes` DISABLE KEYS */;
-INSERT INTO `testes` VALUES (1,'td',NULL,NULL);
-/*!40000 ALTER TABLE `testes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -118,8 +90,52 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'José Nogueira','josenogueira@loba.pt','$2y$10$66LdnGVPtXrigMo14kDeVeQSBU/HVeV9zJF/5dbILTUGjjlY2ojzm','nvQWhQtx86zyLYZUNww5NucT56P7eTLpznEW6QVFm0KbjV5JohOBh7pPfivL','2018-04-26 08:36:06','2018-04-26 08:36:06');
+INSERT INTO `users` VALUES (1,'User Admin','admin@zwcms.com','$2y$10$7DsSX60i3Ej6PrmLjPoGpuGkPw/HFGeg9VWPa1B7U2vxooKAZmnyW','6jLW9K2RG0GrNi7Bsu6AxnSsTCJxncQ8FyVAnxhxQg6jcMKS8SS2djaAUIxv',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `website_settings`
+--
+
+DROP TABLE IF EXISTS `website_settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `website_settings` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `website_name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_account_email` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_legal_name` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_desc` text COLLATE utf8mb4_unicode_ci,
+  `website_phone` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_street` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_city` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_postal_code` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_country` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_region` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_currency` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_iban` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_order_prefix` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_order_suffix` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_url_facebook` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_url_twitter` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_url_instagram` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_url_google` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website_mode_store` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `website_settings`
+--
+
+LOCK TABLES `website_settings` WRITE;
+/*!40000 ALTER TABLE `website_settings` DISABLE KEYS */;
+INSERT INTO `website_settings` VALUES (1,'zwcms','zwcms@mail.com','zwcms','zwcms',NULL,NULL,NULL,NULL,NULL,NULL,'€',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'2018-05-25 14:10:22');
+/*!40000 ALTER TABLE `website_settings` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -131,4 +147,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-18 17:57:20
+-- Dump completed on 2018-05-25 18:15:32
