f36e8dd221d0b62c24c9399d696421f2c9c24a4d
zeworks <josenogueira95@hotmail.com>
2018-06-01 18:26:10 +0100

    update

--- resources/views/admin/templates/_form.blade.php
+++ resources/views/admin/templates/_form.blade.php
@@ -1,82 +1,84 @@
-    @csrf
-    <div class="col-lg-9 col-sm-6 col-xs-12">
-        <div class="table-items">
-            <div class="form-field clearfix">
-                <label for="title">Nome da Página</label>
-                <input type="text" name="title" id="title" class="form-control" required>
-            </div>
-
+@csrf
+<div class="col-lg-9 col-sm-6 col-xs-12">
+    <div class="table-items">
+        <div class="form-field clearfix">
+            <label for="title">Nome da Página</label>
+            <input type="text" name="title" id="title" class="form-control" required value="{{$templatedata->title}}">
         </div>
-        <hr>
-        <h2 class="page-title">Blocos da Página</h2>
-        <div class="table-items">
-            <div class="bloc">
-                <div class="bloc-item clearfix">
-                    <div class="col-xs-12">
-                        <a href="" class="link fright btn-delBloc"><i class="fa fa-trash"></i></a>
-                        <p class="strong">Bloco Nº 1</p>
-                        <div class="form-field">
-                            <label for="title_bloc_page_1">Título do Bloco</label>
-                            <input type="text" name="title_bloc_page" id="title_bloc_page_1" class="form-control">
-                        </div>
-                        <br>
-                        <div class="form-field">
-                            <label for="resum_bloc_item_1">Resumo do Bloco</label>
-                            <input type="text" name="resum_bloc_item" id="resum_bloc_item_1" class="form-control">
-                        </div>
-                        <br>
-                        <div class="form-field">
-                            <label for="desc_bloc_item_1">Descrição do Bloco</label>
-                            <textarea name="desc_bloc_item" id="desc_bloc_item_1" class="text-editor"></textarea>
-                        </div>
+    </div>
+    <hr>
+    <h2 class="page-title">Blocos da Página</h2>
+    <div class="table-items">
+        <div class="bloc">
+            <div class="bloc-item clearfix">
+                <div class="col-xs-12">
+                    <a href="" class="link fright btn-delBloc">
+                        <i class="fa fa-trash"></i>
+                    </a>
+                    <p class="strong">Bloco Nº 1</p>
+                    <div class="form-field">
+                        <label for="title_bloc_page_1">Título do Bloco</label>
+                        <input type="text" name="title_bloc_page" id="title_bloc_page_1" class="form-control">
+                    </div>
+                    <br>
+                    <div class="form-field">
+                        <label for="resum_bloc_item_1">Resumo do Bloco</label>
+                        <input type="text" name="resum_bloc_item" id="resum_bloc_item_1" class="form-control">
+                    </div>
+                    <br>
+                    <div class="form-field">
+                        <label for="desc_bloc_item_1">Descrição do Bloco</label>
+                        <textarea name="desc_bloc_item" id="desc_bloc_item_1" class="text-editor"></textarea>
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-default btn-addBloc"><i class="far fa-plus-square"></i> Adicionar Bloco</button>
         </div>
-        <hr>
+        <button type="button" class="btn btn-default btn-addBloc">
+            <i class="far fa-plus-square"></i> Adicionar Bloco</button>
     </div>
-    <div class="col-lg-3 col-sm-6 col-xs-12">
-        <div class="table-items">
-            <div class="form-field">
-                <span>Estado</span>
-                <br/>
-                <br/>
-                <!-- template status -->
-                <input type="hidden" name="status_hidden" class="status_hidden">
-                <div class="switch">
-                    <input type="checkbox" class="btsp-check" name="status_item">
-                    <span class="toggle toggle--round"></span>
-                </div>
-                <!-- /template status -->
+    <hr>
+</div>
+<div class="col-lg-3 col-sm-6 col-xs-12">
+    <div class="table-items">
+        <div class="form-field">
+            <span>Estado</span>
+            <br/>
+            <br/>
+            <!-- template status -->
+            <input type="hidden" name="status_hidden" class="status_hidden">
+            <div class="switch {{ $templatedata -> status == 'on' ? 'checked' : '' }}">
+                <input type="checkbox" class="btsp-check" name="website_mode_store" {{ $templatedata -> status == 'on' ? 'checked' : '' }}>
+                <span class="toggle toggle--round"></span>
             </div>
-            <div class="form-field">
-                <span>Imagem de Destaque
-                    <strong>1920x520</strong>
-                </span>
-                <br>
-                <br>
-                <!-- previewed image -->
-                <img class="image-previewer">
-                <div class="buttons-top fleft">
-                    <input type="file" name="upload_banner" id="upload_photo" class="hidden">
-                    <a href="#" class="btn btn-default upload" data-target="upload_photo">Enviar Ficheiro</a>
-                </div>
-            </div>
+            <!-- /template status -->
         </div>
-    </div>
-    <div class="buttons-top buttons-top-absolute">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-8 col-lg-10">
-                    <span id="status-info">Dados por guardar</span>
-                </div>
-                <div class="col-sm-2 col-lg-1">
-                    <a href="#" title="Cancelar" class="btn btn-default decline">Cancelar</a>
-                </div>
-                <div class="col-sm-2 col-lg-1">
-                    <button type="submit" title="Guardar" name="save_data" class="btn btn-primary save">Guardar</button>
-                </div>
+        <div class="form-field">
+            <span>Imagem de Destaque
+                <strong>1920x520</strong>
+            </span>
+            <br>
+            <br>
+            <!-- previewed image -->
+            <img class="image-previewer" src="{{ asset('storage/images/'.$templatedata->featured_image) }}">
+            <div class="buttons-top fleft">
+                <input type="file" name="upload_banner" id="upload_photo" class="hidden">
+                <a href="#" class="btn btn-default upload" data-target="upload_photo">Enviar Ficheiro</a>
             </div>
         </div>
     </div>
+</div>
+<div class="buttons-top buttons-top-absolute">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-8 col-lg-10">
+                <span id="status-info">Dados por guardar</span>
+            </div>
+            <div class="col-sm-2 col-lg-1">
+                <a href="#" title="Cancelar" class="btn btn-default decline">Cancelar</a>
+            </div>
+            <div class="col-sm-2 col-lg-1">
+                <button type="submit" title="Guardar" name="save_data" class="btn btn-primary save">Guardar</button>
+            </div>
+        </div>
+    </div>
+</div>
